61d30b108fe4a05bcb274151b9ec134eef790f6b
siTotes <timeminecraft.1140@gmail.com>
2023-02-20 21:05:26 +0700

    m

--- alpha.js
+++ alpha.js
@@ -12,16 +12,17 @@ let client = sheetdb(config);
 //require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
 const cekonlen = setInterval(() => {
     client.read({ limit: 1, infobot: "status" }).then(function(data) {
-    var json = JSON.parse(data);
-    var valuee = json[0].IsOnline;
-    if(valuee=='off'){
-        client.update(
-            'infobot', // column name
-            'status', // value to search for
-            { 'IsOnline': 'on' } // object with updates
-        ).then(function(data) 
+        var json = JSON.parse(data);
+        var valuee = json[0].IsOnline;
+        if(valuee=='off'){
+            client.update(
+                'infobot', // column name
+                'status', // value to search for
+                { 'IsOnline': 'on' } // object with updates
+            ).then(function(data) 
             //console.log(data);
-        
+        }
+    }
 }, 5000);
 
 const {
