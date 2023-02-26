fda0d7676f372b7a75b5b4f6b7ff5c115cbb0717
siTotes <timeminecraft.1140@gmail.com>
2023-02-26 18:53:36 +0700

    m

--- alpha.js
+++ alpha.js
@@ -817,7 +817,7 @@ const { isSetLeft, getTextSetLeft } = require('./lib/setleft')
     * @returns 
     */
    alpha.sendMedia = async (jid, path, filename, quoted = '', options = {}) => {
-      console.log(alpha.user.id)
+      console.log(jid)
       let {
          ext,
          mime,
